79750f2ccfdf54669935003c119b9119089b93cc
oructuran0 <orctrn193@gmail.com>
2024-05-14 23:29:17 +0300

    Service definitions*

--- Core_Proje/BusinessLayer/Abstract/ISkillService.cs
+++ Core_Proje/BusinessLayer/Abstract/ISkillService.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace BusinessLayer.Abstract
 {
-    interface ISkillService
+    public interface ISkillService : IGenericService<Skill>
     {
     }
 }
--- Core_Proje/BusinessLayer/Abstract/ITestimonialService.cs
+++ Core_Proje/BusinessLayer/Abstract/ITestimonialService.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace BusinessLayer.Abstract
 {
-    interface ITestimonialService
+    public interface ITestimonialService : IGenericService<Testimonial>
     {
     }
 }
