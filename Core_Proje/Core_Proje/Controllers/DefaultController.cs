7c005644d96caefb5bdc1336bb37793de267020b
oructuran0 <orctrn193@gmail.com>
2024-05-22 03:27:35 +0300

    SendMessage

--- Core_Proje/Core_Proje/Controllers/DefaultController.cs
+++ Core_Proje/Core_Proje/Controllers/DefaultController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +11,7 @@ namespace Core_Proje.Controllers
 {
     public class DefaultController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
@@ -18,6 +22,20 @@ namespace Core_Proje.Controllers
         }
         public PartialViewResult NavbarPartial()
         {
+            return PartialView();
+        }
+        [HttpGet]
+        public PartialViewResult SendMessage()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult SendMessage(Message p)
+        {
+            MessageManager messageManager = new MessageManager(new EfMessageDal());
+            p.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.Status = true;
+            messageManager.TAdd(p);
             return PartialView();
         }
     }
