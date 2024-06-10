a8b9e35318a20e3751858a99845bdd71c65bce09
oructuran0 <orctrn193@gmail.com>
2024-06-10 23:57:16 +0300

    Experience Delete , Update , Edit => Done

---
+++ Core_Proje/Core_Proje/Controllers/ExperienceController.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Experience List";
+            ViewBag.v2 = "Experiences";
+            ViewBag.v3 = "Experience List";
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience)
+        {
+            experienceManager.TAdd(experience);
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var values = experienceManager.GetByID(id);
+            experienceManager.TDelete(values);
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult EditExperience(int id)
+        {
+            ViewBag.v1 = "Experience List";
+            ViewBag.v2 = "Experiences";
+            ViewBag.v3 = "Experience List";
+            var values = experienceManager.GetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditExperience(Experience experience)
+        {
+            experienceManager.TUpdate(experience);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
