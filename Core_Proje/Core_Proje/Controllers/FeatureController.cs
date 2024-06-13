c4ac76b5ab9dc2f961c2754b4d92ab28ef8b372c
oructuran0 <orctrn193@gmail.com>
2024-06-13 19:37:53 +0300

    Feature Page Update Operations

---
+++ Core_Proje/Core_Proje/Controllers/FeatureController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class FeatureController : Controller
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Edit";
+            ViewBag.v2 = "Highlights";
+            ViewBag.v3 = "Highlight Page";
+            var values = featureManager. TGetByID(1);
+            return View(values);
+
+        }
+        [HttpPost]
+        public IActionResult Index(Feature feature)
+        {
+            featureManager.TUpdate(feature);
+            return RedirectToAction("Index","Default");
+        }
+    }
+}
