6b9db66ef49fc97e73ba6862d8057e8a78cbac4d
oructuran0 <orctrn193@gmail.com>
2024-06-15 04:53:34 +0300

    Dashboard NumericalStatistics panel
    portfolio, message,service count

---
+++ Core_Proje/Core_Proje/ViewComponents/Dashboard/NumericalStatistics.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class NumericalStatistics : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.n1 = c.Portfolios.Count();
+            ViewBag.n2 = c.Messages.Count();
+            ViewBag.n3 = c.Services.Count();
+            return View();
+        }
+    }
+}
