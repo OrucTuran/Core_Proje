c12771f3dba79a92403d0e201406f2289a956573
oructuran0 <orctrn193@gmail.com>
2024-06-30 11:37:12 +0300

    Uploading Images from File

---
+++ Core_Proje/Core_Proje/Areas/Writer/Controllers/ProfileController.cs
@@ -0,0 +1,57 @@
+﻿using Core_Proje.Areas.Writer.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public ProfileController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.PictureURL = values.ImageURL;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Picture!=null)
+            {
+                string resource = Directory.GetCurrentDirectory();
+                string extension = Path.GetExtension(p.Picture.FileName);
+                string imageName = Guid.NewGuid() + extension;
+                string saveLocation = resource + "/wwwroot/UserImage/" + imageName;
+                FileStream stream = new FileStream(saveLocation, FileMode.Create);
+                await p.Picture.CopyToAsync(stream);
+                user.ImageURL = imageName;
+            }
+            user.Name = p.Name;
+            user.Surname = p.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Index), "Default");
+            }
+            return View();
+        }
+    }
+}
