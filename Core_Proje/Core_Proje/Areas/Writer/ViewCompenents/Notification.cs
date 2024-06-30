0a2753c95271d83200768c667407b751c9ed7bd3
oructuran0 <orctrn193@gmail.com>
2024-06-30 16:51:29 +0300

    Dashboard Notifications button settings

---
+++ Core_Proje/Core_Proje/Areas/Writer/ViewCompenents/Notification.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Areas.Writer.ViewCompenents
+{
+    public class Notification : ViewComponent
+    {
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = announcementManager.TGetList();
+            return View(values);
+        }
+    }
+}
