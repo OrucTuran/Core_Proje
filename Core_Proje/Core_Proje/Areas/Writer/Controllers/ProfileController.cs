34676416e7ed757bdd739cbf1dd6502886fa2132
oructuran0 <orctrn193@gmail.com>
2024-07-01 13:23:48 +0300

    dashboard password update operations

--- Core_Proje/Core_Proje/Areas/Writer/Controllers/ProfileController.cs
+++ Core_Proje/Core_Proje/Areas/Writer/Controllers/ProfileController.cs
@@ -46,6 +46,7 @@ namespace Core_Proje.Areas.Writer.Controllers
             }
             user.Name = p.Name;
             user.Surname = p.Surname;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, p.Password);
             var result = await _userManager.UpdateAsync(user);
             if (result.Succeeded)
             {
@@ -55,3 +56,4 @@ namespace Core_Proje.Areas.Writer.Controllers
         }
     }
 }
+//1111Oo+
