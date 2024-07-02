a6d4c193615802eec18645cb2edc42ad8a6ae0c1
oructuran0 <orctrn193@gmail.com>
2024-07-02 17:04:43 +0300

    admin page navbar message list

---
+++ Core_Proje/Core_Proje/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class AdminNavbarMessageList : ViewComponent
+    {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        string p = "admin@gmail.com";
+        public IViewComponentResult Invoke()
+        {
+            var values = writerMessageManager.GetListReceiverMessages(p).OrderByDescending(x=>x.WriterMessageID).Take(3).ToList();
+            return View(values);
+        }
+    }
+}
