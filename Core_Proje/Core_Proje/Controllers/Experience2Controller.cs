26d2a0a662ef6bdb5432b86a76027f6e02351342
oructuran0 <orctrn193@gmail.com>
2024-07-04 17:27:59 +0300

    List operation with ajax
    Experience List

---
+++ Core_Proje/Core_Proje/Controllers/Experience2Controller.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class Experience2Controller : Controller //ajax islemlerini yapacagimiz controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ListExperience()
+        {
+            var values = JsonConvert.SerializeObject(experienceManager.TGetList());
+            return Json(values);
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience p)
+        {
+            experienceManager.TAdd(p);
+            var values = JsonConvert.SerializeObject(p);
+            return Json(values);
+        }
+    }
+}
