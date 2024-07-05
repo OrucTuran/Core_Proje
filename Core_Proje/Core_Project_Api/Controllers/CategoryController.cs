8fd00709b6e9e007c28a4be595c0cdbf469da7c2
oructuran0 <orctrn193@gmail.com>
2024-07-05 13:45:07 +0300

    Get Request with APIs

---
+++ Core_Proje/Core_Project_Api/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Core_Project_Api.DAL.ApiContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Project_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCagegoryList()
+        {
+            var context = new Context();
+            return Ok(context.Categories.ToList());
+        }
+    }
+}
